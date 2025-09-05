3b0722878b4629ad54c2203441f13952a467f470
EMAM  AL MOUNQER <86230225+emammounqer@users.noreply.github.com>
2025-09-05 19:14:45 +0300

    build: add lib react query and axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "./App.css";
+import Home from "./pages/home/Home";
+
+const queryClient = new QueryClient();
 
 function App() {
-  return <></>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Home />
+    </QueryClientProvider>
+  );
 }
 
 export default App;
